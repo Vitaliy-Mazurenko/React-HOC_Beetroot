a6d58ea4772184c4a3bf643bf6e764445757f4ad
romannikiforov <escaro@ukr.net>
2021-09-27 21:20:13 +0300

    HOC start

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,31 @@
 import {Component} from "react"
 
+
+const users = ['Bill', 'Tom']
+
+const List = () => <ul>
+  {users.map(user => <li key={user}>{user}</li>)}
+</ul>
+
+const Message = () => <p>this is paragraph</p> 
+
+
+const withTitle = BC => props => <>
+  <h1>{props.title}</h1>
+  <BC {...props}/>
+</>
+
+const TitledList = withTitle(List)
+const TitledMessage = withTitle(Message)
+
+
 class App extends Component {
   render() {
     return (
       <div className="container">
-        <h1>Hello React</h1>
+        <List />
+        <TitledList title="My new List"/>
+        <TitledMessage title="My message"/>
       </div>
     )
   }
